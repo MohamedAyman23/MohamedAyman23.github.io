16f9851dee1cd08c9e0b4584003551f080d4a298
Mohammed Ayman <mohamedaymanmohamedatia@gmail.com>
2026-08-14 02:06:48 +0300

    update the next.config

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
   output: "export",
-
   trailingSlash: true,
-
   images: {
     unoptimized: true,
   },
+  // Set the base path to match the GitHub repository name for correct asset loading
+  basePath: "/MohamedAyman23.github.io", 
 };
 
 export default nextConfig;
